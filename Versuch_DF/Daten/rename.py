9769ecfdeed1a86f1fe5e6bfa0751c4916342c5d
ManeLippert <manuel-lippert@outlook.de>
2023-03-28 22:02:18 +0200

    #1: Protokoll Versuch DF

--- Versuch_DF/Daten/rename.py
+++
@@ -1,7 +0,0 @@
-import os
-
-data = "Reflexion"
-
-for d in os.listdir(data):
-    for f in os.listdir(data + "/" + d):
-        os.rename(data + "/" + d + "/" + f, data + "/" + d + "/" + f.replace("Relexion", "Reflexion"))
